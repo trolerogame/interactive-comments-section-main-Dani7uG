b6082fe58b20628a9ecfabd08b9afce9c3fd3cf9
Daniel Campos <danielgcampos.04@gmail.com>
2021-12-26 17:59:56 -0300

    homepage agreagado, script para produccion agregados y base de vite cambiada

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base:'/interactive-comments-section-main-Dani7uG'
 })
